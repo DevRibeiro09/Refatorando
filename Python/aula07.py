6c744c28cb44372c9819c7b6667b5e70818d2b0a
DevRibeiro09 <contatodevribeiro@gmail.com>
2024-02-19 22:32:32 -0300

    Aula 07
    Big Mac & Maior Num

--- Python/aula07.py
+++ Python/aula07.py
@@ -1,7 +1,23 @@
 # FUNÇÕES EM PYTHON
 # COLEÇÃO DE CÓDIGO/LÓGICA
-def bigmac():
+""" def bigmac():
     print('Sanduíche Big Mac Picanha')
 print('Início')
 bigmac()
-print('Fim')
+print('Fim') """
+
+def fazer_big_mac(nome):
+    print(f"Sanduíche Big Mac {nome}")
+    
+fazer_big_mac('Roger')
+fazer_big_mac('Cris')
+fazer_big_mac('João')
+
+def maior_num(lista_num):
+    lista_num.sort()
+    lista_num.reverse()
+    maior_num = lista_num[0]
+    return maior_num
+
+resultado = maior_num([312,912,418,412,617,867,575,1028,-8,-10,382])
+print(resultado)
